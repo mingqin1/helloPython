474070e7c21b8460816fd57723ecd6e390a72fb9
Ming Qin (ming.qin1@navy.mil), Matt Bates (matthew.c.bates1@navy.mil),  Daniel Hernandez(daniel.hernandez11@navy.mil) <>
2020-04-21 08:55:57 -0700

    Modern Replacements for map, filter, and reduce

--- chapter_5_First_Class_Functions/functionAsObject.py
+++ chapter_5_First_Class_Functions/functionAsObject.py
@@ -1,3 +1,5 @@
+from functools import reduce
+from operator import add
 
 
 def factorial(n):
@@ -32,3 +34,19 @@ print(sorted(fruits, key=len))
 
 # Sorting a list of words by their reversed spelling
 print(sorted(fruits, key=reverse))
+
+# Modern Replacements for map, filter, and reduce
+# using map -Build a list of factorials from 0! to 5!.
+list(map(fact, range(6)))
+# using list comprehension
+[fact(n) for n in range(6)]
+
+# using filter and map -List of factorials of odd numbers up to 5!,
+list(map(factorial, filter(lambda n: n % 2, range(6))))
+# using list comprehesnion
+[factorial(n) for n in range(6) if n % 2]
+
+# using reduce -Sum of integers up to 99 performed with reduce and sum
+reduce(add, range(100))
+# using sum
+sum(range(100))
