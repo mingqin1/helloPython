bbba897a42643d38948ef5e150a41b495b72fe1c
Ming Qin (ming.qin1@navy.mil), Matt Bates (matthew.c.bates1@navy.mil),  Daniel Hernandez(daniel.hernandez11@navy.mil) <>
2020-04-23 08:57:57 -0700

    updates

--- chapter_2_An_Array_of_Sequences/memoryview.py
+++ chapter_2_An_Array_of_Sequences/memoryview.py
@@ -1,10 +1,21 @@
 import array as array
 
+"""
+A memoryview is essentially a generalized NumPy array structure in Python itself (without the math).
+It allows you to share memory between data-structures (things like PIL images, SQLlite databases, NumPy arrays, etc.)
+without first copying. This is very important for large data sets.
+"""
+
 numbers = array.array('h', [-2, -1, 0, 1, 2])
 mem = memoryview(numbers)
 len(mem)
 
 mem[0]
+
+"""
+memoryview.cast method lets you change the way multiple bytes are read or written as units without moving bits around
+"""
+# an example of changing a single byte of an array of 16-bit integers.
 
 mem_oct = mem.cast('B')
 
